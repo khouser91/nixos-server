4eedfd0ac75e45c7d4534814a0c9424a7110e4fe
khouser91 <khouse3280@gmail.com>
2025-09-15 19:45:34 -0400

    add reverse proxy to caddy

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -6,5 +6,29 @@
 		virtualHosts."localhost".extraConfig = ''
 			respond "Hello, world!"
 		'';
+
+		virtualHosts."jellyfin.houserkaser.local".extraConfig = ''
+			reverse_proxy 10.0.1.40:8096
+		'';
+
+		virtualHosts."radarr.houserkaser.local".extraConfig = ''
+			reverse_proxy 10.0.1.20:7878
+		'';
+
+		virtualHosts."sonarr.houserkaser.local".extraConfig = ''
+			reverse_proxy 10.0.1.20:8989
+		'';
+
+		virtualHosts."qbit.houserkaser.local".extraConfig = ''
+			reverse_proxy 10.0.1.20:8080
+		'';
+
+		virtualHosts."prowlarr.houserkaser.local".extraConfig = ''
+			reverse_proxy 10.0.1.20:9696
+		'';
+
+		virtualHosts."hass.houserkaser.local".extraConfig = ''
+			reverse_proxy 10.0.1.10:8123
+		'';
 	};
 }
