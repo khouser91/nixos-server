d37a36e13626cce699be981ce997e4f07dcb0faf
khouser91 <khouse3280@gmail.com>
2025-09-28 17:10:00 -0400

    add newt

--- hosts/desktop-server/configuration.nix
+++ hosts/desktop-server/configuration.nix
@@ -12,7 +12,7 @@
       ../../modules/filesystem.nix
       ../../modules/jellyfin.nix
       ../../modules/caddy.nix
-      ../../pangolin/newt.nix
+      ../../modules/pangolin/newt.nix
     ];
 
   # Bootloader.
